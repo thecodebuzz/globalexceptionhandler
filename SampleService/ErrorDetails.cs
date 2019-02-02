86b71651d5f4d3c4d50cb406798903050fe8aa29
CodeBuzz <codebuzz.net@gmail.com>
2019-02-02 10:04:09 -0500

    Add project files.

---
+++ SampleService/ErrorDetails.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace SampleService
+{
+    internal class ErrorDetails
+    {
+        public ErrorDetails()
+        {
+        }
+
+        public int StatusCodeContext { get; internal set; }
+        public string Message { get; internal set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ SampleService/GlobalExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace SampleService
+{
+    public static class GlobalExceptionMiddleware
+    {
+        public static void UseApiExceptionHandler(this IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if(contextFeature!=null)
+                    {
+                        //log the error
+                        var logger = loggerFactory.CreateLogger("GlobalException");
+                        logger.LogError($"Something went wrong: { contextFeature.Error}");
+
+                        //report the error to client 
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCodeContext = context.Response.StatusCode,
+                            Message ="Something Went Wrong.Please try again later"
+
+
+                        }.ToString());
+                    }
+
+                });
+
+             });
+
+        }
+    }
+}
